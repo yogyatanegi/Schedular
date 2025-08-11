0015de84c381ad79596ef9e66bed06d7a2a239b7
yogyata negi <156119601+yogyatanegi@users.noreply.github.com>
2025-08-11 21:41:19 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ['canvasjs-react-charts']
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Change `Scheduler` to your repo name exactly
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['canvasjs-react-charts']
-  }
+  base: '/Scheduler/',
 })
